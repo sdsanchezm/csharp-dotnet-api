3c70bb2de477f43bdfbfa3ba243c169a2df7ae28
sdsanchezm <sdsanchezm@gmail.com>
2023-09-06 13:04:52 -0500

    Created the ToDo model

--- webapi_project_functional/Models/ToDo.cs
+++ webapi_project_functional/Models/ToDo.cs
@@ -1,7 +1,22 @@
-﻿namespace webapi_project_functional.Models
+﻿
+
+namespace webapi_project_functional.Models
 {
     public class ToDo
     {
+        public Guid ToDoId { get; set; }
+        public string ToDoName { get; set; }
+        public string ToDoDescription { get; set; }
+        public Priority ToDoPriority { get; set; }
+        public DateTime ToDoCreationDate { get; set; }
+        public string Summary { get; set; }
 
+    }
+
+    public enum Priority
+    {
+        High,
+        Medium,
+        Low
     }
 }
