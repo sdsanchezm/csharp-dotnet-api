213a72271b4dcf7aacf671f56b66a4138fae6aa2
sdsanchezm <sdsanchezm@gmail.com>
2023-09-06 13:07:55 -0500

    adjusting foreign keys of ToDo Model

--- webapi_project_functional/Models/ToDo.cs
+++ webapi_project_functional/Models/ToDo.cs
@@ -5,10 +5,12 @@ namespace webapi_project_functional.Models
     public class ToDo
     {
         public Guid ToDoId { get; set; }
+        public Guid CategoryId { get; set; }
         public string ToDoName { get; set; }
         public string ToDoDescription { get; set; }
         public Priority ToDoPriority { get; set; }
         public DateTime ToDoCreationDate { get; set; }
+        public virtual Category Category { get; set; }
         public string Summary { get; set; }
 
     }
