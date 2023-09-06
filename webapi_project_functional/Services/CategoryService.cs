fddb03d242c69fe418fb4fe3b1ffa2a924ab12ea
sdsanchezm <sdsanchezm@gmail.com>
2023-09-06 13:43:24 -0500

    Created the Category Service and Category Interface

---
+++ webapi_project_functional/Services/CategoryService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Update.Internal;
+using webapi_project_functional.Models;
+
+namespace webapi_project_functional.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        ToDoContext _dbContext;
+
+        public CategoryService(ToDoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Category> Get()
+        {
+            return _dbContext.Categories;
+        }
+
+        public void Save(Category c)
+        {
+            _dbContext.Add(c);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task Update(Guid id, Category c)
+        {
+            var actualCategory = _dbContext.Categories.Find(id);
+
+            if (actualCategory != null)
+            {
+                actualCategory.CategoryName = c.CategoryName;
+                actualCategory.CategoryDescription = c.CategoryDescription;
+                actualCategory.CategoryLevel = c.CategoryLevel;
+
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            var actualCategory = _dbContext.Categories.Find(id);
+            if (actualCategory != null)
+            {
+                _dbContext.Remove(actualCategory);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+
+    public interface ICategoryService
+    {
+        IEnumerable<Category> Get();
+        void Save(Category c);
+        Task Update(Guid id, Category c);
+        void Delete(Guid id);
+    }
+}
