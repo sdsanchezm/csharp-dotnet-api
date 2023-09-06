2670b79a5fb118fb1a9fb79ce0e857b31911ac47
sdsanchezm <sdsanchezm@gmail.com>
2023-09-06 13:06:13 -0500

    Created the Category model and adjusting ToDo Model

---
+++ webapi_project_functional/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace webapi_project_functional.Models
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public int CategoryLevel { get; set; }
+
+        //[JsonIgnore]
+        public virtual ICollection<ToDo> ToDos { get; set; }
+    }
+}
