25f17ca29aa5c538b1c23ff2d1fdb9cc70437cc5
sdsanchezm <sdsanchezm@gmail.com>
2023-09-06 14:02:16 -0500

    Created CategoryController using Dependency Injection from the Service Interface

---
+++ webapi_project_functional/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi_project_functional.Models;
+using webapi_project_functional.Services;
+
+namespace webapi_project_functional.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        ICategoryService _categoryServiceLocal;
+        public CategoryController(ICategoryService categoryServiceLocal)
+        {
+            _categoryServiceLocal = categoryServiceLocal;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_categoryServiceLocal.Get());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Category c)
+        {
+            _categoryServiceLocal.Save(c);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Category c, Guid id)
+        {
+            _categoryServiceLocal.Update(id, c);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            _categoryServiceLocal.Delete(id);
+            return Ok();
+        }
+
+
+    }
+}
