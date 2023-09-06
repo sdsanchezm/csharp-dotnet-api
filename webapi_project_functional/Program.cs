dc2b6b9ff9d7935740bf2b156b3132c5b1ef8d1c
sdsanchezm <sdsanchezm@gmail.com>
2023-09-06 14:40:20 -0500

    created connection string, connected to the Database (CodeFirst Approach), initial test ok

--- webapi_project_functional/Program.cs
+++ webapi_project_functional/Program.cs
@@ -1,18 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using webapi_project_functional;
+using webapi_project_functional.Services;
+using webapi_project_functional.Controllers;
 
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        //builder.Services.AddSqlServer<ToDoContext>("Data Source=KRAUSP52\\SQLEXPRESS;Initial Catalog=ToDosDataB1;Trusted_Connection=True;TrustServerCertificate=true;");
+        //builder.Services.AddSqlServer<ToDoContext>(builder.Configuration.GetConnectionString("connString1"));
+        builder.Services.AddSqlServer<ToDoContext>(builder.Configuration.GetConnectionString("connString2"));
+        builder.Services.AddScoped<ICategoryService, CategoryService>();
+        builder.Services.AddScoped<IToDoService, ToDoService>();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
